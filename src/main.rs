9060da73728129718d7b20266cc330ca1cdd7c2c
krobbi <70552146+krobbi@users.noreply.github.com>
2026-04-27 19:57:18 +0100

    Read source code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
-use std::{env, path::Path, process::ExitCode};
+use std::{env, fs, path::Path, process::ExitCode};
 
 /// Runs Lox and returns an [`ExitCode`].
 fn main() -> ExitCode {
     let mut args = env::args_os().skip(1);
 
-    if args.len() == 1 {
-        let path = args.next().expect("argument count should be checked");
-        let path = Path::new(&path);
-        println!("Path: {:?}", path.to_string_lossy());
-        ExitCode::SUCCESS
-    } else {
+    if args.len() != 1 {
         eprintln!("Usage: lox <path>");
-        ExitCode::FAILURE
+        return ExitCode::FAILURE;
     }
+
+    let path = args.next().expect("argument count should be checked");
+    let path = Path::new(&path);
+
+    let Ok(source) = fs::read_to_string(path) else {
+        eprintln!("Could not read file {:?}.", path.to_string_lossy());
+        return ExitCode::FAILURE;
+    };
+
+    interpret_source(&source);
+    ExitCode::SUCCESS
+}
+
+/// Interprets source code.
+fn interpret_source(source: &str) {
+    println!("Source: {source:?}");
 }
