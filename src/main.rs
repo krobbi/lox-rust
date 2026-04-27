cba8a3a9a92c1f8029590e0de6b935ebf88da538
krobbi <70552146+krobbi@users.noreply.github.com>
2026-04-27 17:04:15 +0100

    Parse command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
-/// Runs Lox.
-fn main() {
-    println!("Hello, Lox!");
+use std::{env, path::Path, process::ExitCode};
+
+/// Runs Lox and returns an [`ExitCode`].
+fn main() -> ExitCode {
+    let mut args = env::args_os().skip(1);
+
+    if args.len() == 1 {
+        let path = args.next().expect("argument count should be checked");
+        let path = Path::new(&path);
+        println!("Path: {:?}", path.to_string_lossy());
+        ExitCode::SUCCESS
+    } else {
+        eprintln!("Usage: lox <path>");
+        ExitCode::FAILURE
+    }
 }
